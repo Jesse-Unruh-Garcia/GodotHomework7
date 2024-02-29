c7c220a93cd977bb5bfbbfc43e27db2f9bd6433a
Jesse-Unruh-Garcia <98713768+Jesse-Unruh-Garcia@users.noreply.github.com>
2024-02-29 13:42:08 -0500

    Yayayay

---
+++ godot-buttons-and-signals-main/Scripts/Scripts Only for Homework Exercises/CollisionController_HW_four.gd
@@ -0,0 +1,44 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _disable_collision():
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func _enable_collision():
+	$CollisionShape2D.set_deferred("disabled", false)
+
+
+
+
+
+
+
+
+func _on_area_2d_body_entered(body):
+	_disable_collision()
+	pass # Replace with function body.
+
+
+
+
+func _on_area_2d_2_body_entered(body):
+	_enable_collision()
+	pass # Replace with function body.
+	
+	
+
+
+func _on_area_2d_3_body_entered(body):
+	_disable_collision()
+	pass # Replace with function body.
+	
+	
+
+
+func _on_area_2d_3_body_exited(body):
+	_enable_collision()
+	pass # Replace with function body.
